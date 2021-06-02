8e0ed7128187a7a5c1224666562895d7b5b93266
kjelazkov <kjelyazkovnew@gmail.com>
2021-06-02 08:14:23 +0300

    tailwind

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/public/main.css">
 </head>
 <body>
     <ul>
-        <li><a href="/">Index</a></li>
+        <li class="bg-red-400"><a href="/">Index</a></li>
         <li><a href="/home">Home</a></li>
         <li><a href="/about">About</a></li>
     </ul>
